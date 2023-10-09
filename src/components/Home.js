60023d2ce6ab47524c4b64d0fdd2300e2602a9f5
Dinesh <dinesh.melapundi@gmail.com>
2023-10-09 19:09:34 +0530

    added follow tile in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,14 +31,32 @@ const Home = () => {
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBiKuQD1roBYu8L_aLOt4ZvytuNFNMRWmtZw&usqp=CAU",
       price: 29.99,
     },
-    // {
-    //   id: 3,
-    //   name: "Ask",
-    //   description: "Pen your opinion here",
-    //   //   imageUrl: "path_to_another_image.jpg",
-    //   price: 29.99,
-    // },
+    {
+      id: 4,
+      name: "Follow",
+      description: "Get to know your followers here",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3H4inCxRkr6GQgH6c8KmQtW52sCvfBQgLeBnpsf7RTER5T2BngbHBB92VAiMe74QF7V0&usqp=CAU",
+      price: 1.0,
+    },
   ];
+  const chunkArray = (array, size) => {
+    let results = [];
+    while (array.length) {
+      results.push(array.splice(0, size));
+    }
+    return results;
+  };
+
+  // Decide on chunk size based on number of products
+  let chunkSize = 3;
+  if (products.length === 4) {
+    chunkSize = 2;
+  } else if (products.length === 5) {
+    chunkSize = 3; // it will automatically split to 3 and 2
+  }
+
+  const chunkedProducts = chunkArray([...products], chunkSize);
   return (
     <div className="home_div">
       {/* <h2>Welcome to Universe </h2> */}
@@ -49,11 +67,13 @@ const Home = () => {
 
       <p className="home_para">Web Components that just works !</p>
       <div className="home_container">
-        <div className="home_row">
-          {products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-        </div>
+        {chunkedProducts.map((chunk, index) => (
+          <div className="home_row row" key={index}>
+            {chunk.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
+        ))}
         <div className="home_testimonials">
           <h2 className="testimonial-heading">
             Don't Take Our Words See Who Are Using Ore Web Products
