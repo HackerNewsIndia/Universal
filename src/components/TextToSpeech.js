7b1263d423010fd3b2677217536ec5509ae84ac8
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2023-10-17 22:04:18 +0530

    Create TextToSpeech.js

---
+++ src/components/TextToSpeech.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css"; // Replace "styles.css" with the path to your CSS file
+
+
+const TextToSpeech = ({ text }) => {
+  const [isPaused, setIsPaused] = useState(false);
+  const [utterance, setUtterance] = useState(null);
+  const [selectedVoice, setSelectedVoice] = useState(null);
+
+  const u = new SpeechSynthesisUtterance(text); // Define the utterance outside useEffect
+
+  useEffect(() => {
+    const synth = window.speechSynthesis;
+    
+    const handleVoicesChanged = () => {
+      const voices = synth.getVoices();
+      console.log(voices); // Log available voices for debugging
+
+      // You can create a list of voices to display to the user and let them select one
+      // For simplicity, you can select the first voice available here
+      if (voices.length > 0) {
+        setSelectedVoice(voices[0]);
+        u.voice = voices[0];
+        setUtterance(u);
+      }
+    };
+
+    synth.addEventListener("voiceschanged", handleVoicesChanged);
+
+    return () => {
+      synth.cancel();
+      synth.removeEventListener("voiceschanged", handleVoicesChanged);
+    };
+  }, [text]);
+
+  const handlePlay = () => {
+    const synth = window.speechSynthesis;
+
+    if (isPaused) {
+      synth.resume();
+    }
+
+    if (selectedVoice) {
+      u.voice = selectedVoice;
+    }
+
+    synth.speak(u); // Use the defined `u` variable here
+
+    setIsPaused(false);
+  };
+
+  const handlePause = () => {
+    const synth = window.speechSynthesis;
+
+    synth.pause();
+
+    setIsPaused(true);
+  };
+
+  const handleStop = () => {
+    const synth = window.speechSynthesis;
+
+    synth.cancel();
+
+    setIsPaused(false);
+  };
+
+  return (
+    <div>
+           
+      <button onClick={handlePlay}>{isPaused ? "Resume" : "Play"}</button>
+      <button onClick={handlePause}>Pause</button>
+      <button onClick={handleStop}>Stop</button>
+    </div>
+  );
+};
+
+export default TextToSpeech;
