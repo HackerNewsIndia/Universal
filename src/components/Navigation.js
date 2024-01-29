7035b2f3bdfac1d8548cc877fedc2ca6670af4b6
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-01-29 20:47:22 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,55 +1,170 @@
-// Navigation.js
-import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import "./Navigation.css";
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 function Navigation({ isLoggedIn, setIsLoggedIn }) {
   const location = useLocation();
   const navigate = useNavigate();
   const { pathname } = location;
 
-  const logout = () => {
-    localStorage.removeItem("token");
-    setIsLoggedIn(false);
-    navigate("/");
+  // Add state and toggle function
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  console.log("Is user logged in:", isLoggedIn);
+  const logout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    navigate('/');
+  };
+
+  console.log('Is user logged in:', isLoggedIn);
+
   return (
-    <div className="navigation_div">
-      <h1 className="navigation_h1">UNIVERSE</h1>
-
-      <nav className="navigation_nav">
-        <Link className="home_link" to="/">
-          <i className="fas fa-home"></i> Home
-        </Link>
-
-        <div className="center_nav_items">
-          {!isLoggedIn &&
-            pathname !== "/login" &&
-            pathname !== "/register" &&
-            pathname !== "/dashboard" && (
-              <>
-                <Link className="login_link" to="/login">
-                  <i className="fas fa-sign-in-alt"></i> Login
-                </Link>
-                <Link className="register_link" to="/register">
-                  <i className="fas fa-user-plus"></i> Register
-                </Link>
-                {/* <Link className="view_post_link" to="/diaryblog">
-                  <i className="fas fa-edit"></i> View Posts
-                </Link> */}
-              </>
-            )}
-
-          {isLoggedIn && (
-            <button className="logout_button" onClick={logout}>
-              <i class="fa-solid fa-right-from-bracket"></i> Logout
+    <nav className="bg-blue-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              type="button"
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded={isMobileMenuOpen}
+              onClick={toggleMobileMenu}
+            >
+              <span className="absolute -inset-0.5"></span>
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className={`block h-6 w-6 ${isMobileMenuOpen ? 'hidden' : 'block'}`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              <svg
+                className={`hidden h-6 w-6 ${isMobileMenuOpen ? 'block' : 'hidden'}`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
             </button>
-          )}
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+             
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <a
+                  href="#"
+                  className="bg-gray-900 text-white rounded-md px-3 py-2 text-md font-medium"
+                  aria-current="page"
+                >
+                  Universe
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-medium"
+                >
+                  Products
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-medium"
+                >
+                  About
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+  <Link
+    to="/login" // Specify the path to your login.js file
+    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-medium"
+    onClick={() => setIsMobileMenuOpen(false)} // Close the mobile menu when the login button is clicked
+  >
+    Login
+  </Link>
+  
+            <button
+              type="button"
+              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <span className="absolute -inset-1.5"></span>
+              <span className="sr-only">View notifications</span>
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                />
+              </svg>
+            </button>
+            <div className="relative ml-3">
+              <button
+                type="button"
+                className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                id="user-menu-button"
+                aria-expanded="false"
+                aria-haspopup="true"
+              >
+                <span className="absolute -inset-1.5"></span>
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="h-8 w-8 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </button>
+            </div>
+          </div>
         </div>
-      </nav>
-    </div>
+      </div>
+      <div className={`sm:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          <a
+            href="#"
+            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+            aria-current="page"
+          >
+            Dashboard
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Team
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Projects
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Calendar
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
 
