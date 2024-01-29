d856f47211fdefacbcaee00a13e79d06ed3d3612
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-01-29 20:45:11 +0530

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../index.css";
 
 function Login({ onLogin }) {
@@ -72,13 +72,10 @@ function Login({ onLogin }) {
   };
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full items-center justify-center px-6 py-12 lg:px-8">
+    <div className="bg-white rounded-lg shadow-md p-8 max-w-md w-full">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
+      
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
@@ -89,7 +86,7 @@ function Login({ onLogin }) {
           <div>
             <label
               htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
+              className="block text-md font-medium leading-6 text-gray-900"
             >
               Email address
             </label>
@@ -113,11 +110,11 @@ function Login({ onLogin }) {
             <div className="flex items-center justify-between">
               <label
                 htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="block text-md font-medium leading-6 text-gray-900"
               >
                 Password
               </label>
-              <div className="text-sm">
+              <div className="text-md">
                 <a
                   href="#"
                   className="font-semibold text-indigo-600 hover:text-indigo-500"
@@ -145,23 +142,24 @@ function Login({ onLogin }) {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Sign in
             </button>
           </div>
         </form>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{" "}
-          <a
-            to="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Start a 14 day free trial
-          </a>
-        </p>
+        <p className="mt-10 text-center text-md text-gray-500">
+        Don't have an account?{" "}
+  <Link
+    to="/register"
+    className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+  >
+    Register
+  </Link>
+</p>
       </div>
+    </div>
     </div>
   );
 }
