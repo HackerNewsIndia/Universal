892f67ee3b9e7e81675204aa2bc50426d6fa9ba9
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-01-30 21:27:26 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -74,7 +74,7 @@ function Navigation({ isLoggedIn, setIsLoggedIn }) {
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-medium"
                 >
-                  Products
+                  Product
                 </a>
                 <a
                   href="#"
