5dd844107c240d592be642d1841504a1fc706fbb
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-01-30 20:55:34 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -80,7 +80,7 @@ function Navigation({ isLoggedIn, setIsLoggedIn }) {
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-medium"
                 >
-                  About
+                   Pricing
                 </a>
               </div>
             </div>
