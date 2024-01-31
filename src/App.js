fe3966ba7a990c12f348b75d48d8c371651ea8cb
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-01-31 19:22:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AuthRoutes from "./components/AuthRoutes";
 import Navigation from "./components/Navigation";
-import Product from './components/Product';
+import Product from "./components/Product";
 
 import "./App.css";
 
