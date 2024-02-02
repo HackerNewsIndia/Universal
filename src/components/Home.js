04e9846c79041d283b953241dd455c1b50c3974b
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-02-02 13:44:23 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Product from "./Product";
+import Pricing from "./Pricing";
 import Testimonials from "./Testimonials";
 //import "./home.css"; // If you have additional custom styles
 
