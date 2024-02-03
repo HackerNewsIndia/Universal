00d520897a37f16386c0ba8b1fad19c630f2d89f
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-02-03 21:50:59 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -89,7 +89,7 @@ function Navigation({ isLoggedIn, setIsLoggedIn }) {
               <a
       href="#"
       className={`text-gray-100 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-semibold ${
-        pathname === '/' ? 'bg-gray-900 text-white' : ''
+        pathname === '/' ? 'bg-gray-700 text-white' : ''
       }`}
       onClick={handleUniverseClick}
     >
