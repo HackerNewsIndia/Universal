4fe54581596e3723e159ea2d70994a9332f03f93
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-02-04 21:00:08 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
 import Product from "./Product";
 import Testimonials from "./Testimonials";
 import Pricing from "./Pricing";
@@ -67,18 +69,18 @@ const Home = () => {
             Get a jumpstart to creating your new digital self-expression! With our fully responsive and carefully styled components, you can get the structure of your Blog, Poll, and Personal Journal done with just a couple of clicks.
           </p>
         </div>
-        <div className="container grid justify-center gap-4 mx-auto lg:grid-cols-2 xl:grid-cols-3">
-          {/* Similar structure for the other div elements */}
-          <div class="flex flex-col px-8 py-6">
-			<h2 class="mb-2 text-lg font-semibold sm:text-xl title-font dark:text-gray-100">DiaryBlog</h2>
-			<p class="flex-1 mb-4 text-base leadi dark:text-gray-400">Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community.</p>
-			<a class="inline-flex items-center space-x-2 text-sm dark:text-violet-400" href="/components">
-				<span>Learn More</span>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-					<path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-				</svg>
-			</a>
-		</div>
+    <div className="container grid justify-center gap-4 mx-auto lg:grid-cols-2 xl:grid-cols-3">
+  {/* Similar structure for the other div elements */}
+  <div className="flex flex-col px-8 py-6">
+    <h2 className="mb-2 text-lg font-semibold sm:text-xl title-font dark:text-gray-100">DiaryBlog</h2>
+    <p className="flex-1 mb-4 text-base leadi dark:text-gray-400">Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community.</p>
+    <Link to="/product" className="inline-flex items-center space-x-2 text-sm dark:text-violet-400">
+      <span>Learn More</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+        <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+      </svg>
+    </Link>
+  </div>
     
     
     
@@ -88,43 +90,43 @@ const Home = () => {
     <div class="flex flex-col px-8 py-6 lg:border-none xl:border-solid">
 			<h2 class="mb-2 text-lg font-semibold sm:text-xl title-font dark:text-gray-100">Ask</h2>
 			<p class="flex-1 mb-4 text-base leadi dark:text-gray-400">Gathering opinions or feedback from individuals through surveys or questionnaires to gauge public sentiment or make informed decisions on specific topics.</p>
-			<a class="inline-flex items-center space-x-2 text-sm dark:text-violet-400" href="/sections">
-				<span>Learn More</span>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-					<path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-				</svg>
-			</a>
+			<Link to="/product" className="inline-flex items-center space-x-2 text-sm dark:text-violet-400">
+      <span>Learn More</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+        <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+      </svg>
+    </Link>
 		</div>
 		<div class="flex flex-col px-8 py-6">
 			<h2 class="mb-2 text-lg font-semibold sm:text-xl title-font dark:text-gray-100">My Thoughts</h2>
 			<p class="flex-1 mb-4 text-base leadi dark:text-gray-400">Documenting personal experiences, thoughts, and emotions in a private or public format, providing a space for reflection, self-expression, and personal growth.</p>
-			<a class="inline-flex items-center space-x-2 text-sm dark:text-violet-400" href="/templates">
-				<span>Learn More</span>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-					<path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-				</svg>
-			</a>
+			<Link to="/product" className="inline-flex items-center space-x-2 text-sm dark:text-violet-400">
+      <span>Learn More</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+        <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+      </svg>
+    </Link>
 		</div>
     
 		<div class="flex flex-col px-8 py-6 lg:border-none xl:border-solid">
 			<h2 class="mb-2 text-lg font-semibold sm:text-xl title-font dark:text-gray-100">TypeIt</h2>
 			<p class="flex-1 mb-4 text-base leadi dark:text-gray-400">Pre-made building blocks that you can stack on top of each other like Legos to build a website of your own in minutes.</p>
-			<a class="inline-flex items-center space-x-2 text-sm dark:text-violet-400" href="/sections">
-				<span>Learn More</span>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-					<path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-				</svg>
-			</a>
+			<Link to="/product" className="inline-flex items-center space-x-2 text-sm dark:text-violet-400">
+      <span>Learn More</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+        <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+      </svg>
+    </Link>
 		</div>
 		<div class="flex flex-col px-8 py-6">
 			<h2 class="mb-2 text-lg font-semibold sm:text-xl title-font dark:text-gray-100">Follow</h2>
 			<p class="flex-1 mb-4 text-base leadi dark:text-gray-400">Full pages that showcase pieces of what you can achieve with the building blocks that are in this UI kit.</p>
-			<a class="inline-flex items-center space-x-2 text-sm dark:text-violet-400" href="/templates">
-				<span>Learn More</span>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-					<path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-				</svg>
-			</a>
+			<Link to="/product" className="inline-flex items-center space-x-2 text-sm dark:text-violet-400">
+      <span>Learn More</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+        <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+      </svg>
+    </Link>
 		</div>
 		
         </div>
