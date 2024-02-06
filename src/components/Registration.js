d6d178358ff9fdc3329f64a9336e1d85d3803259
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-02-06 12:12:32 +0530

    Update Registration.js

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,44 +1,34 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Register({ onRegister, setUser: setGlobalUser }) {
   const [user, setUser] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    profession: "",
-    license: "",
-    diaryblogAccess: false,
-    typeitAccess: false,
+    username: '',
+    email: '',
+    password: '',
   });
-  const [errors, setErrors] = useState("");
+  const [errors, setErrors] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
   function validateFields() {
     let validationErrors = {};
 
-    if (!user.username.trim()) validationErrors.username = "Name is required.";
+    if (!user.username.trim()) validationErrors.username = 'Name is required.';
 
     if (!/\S+@\S+\.\S+/.test(user.email))
-      validationErrors.email = "Invalid email.";
+      validationErrors.email = 'Invalid email.';
 
     if (user.password.length < 8)
-      validationErrors.password = "Password must be at least 8 characters.";
-
-    if (user.password !== user.confirmPassword)
-      validationErrors.confirmPassword = "Passwords do not match.";
+      validationErrors.password = 'Password must be at least 8 characters.';
 
     return validationErrors;
   }
 
   const handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
-  };
-  const handleCheckboxChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.checked });
   };
 
   const handleSubmit = async (e) => {
@@ -50,118 +40,125 @@ function Register({ onRegister, setUser: setGlobalUser }) {
       return;
     }
 
+    setLoading(true);
+
     try {
       const res = await axios.post(
-        "https://usermgtapi3.onrender.com/api/register",
+        'https://usermgtapi3.onrender.com/api/register',
         user
       );
       console.log(res.data);
 
       if (res.data.token) {
-        localStorage.setItem("token", res.data.token);
+        localStorage.setItem('token', res.data.token);
         axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${localStorage.getItem("token")}`;
+          'Authorization'
+        ] = `Bearer ${localStorage.getItem('token')}`;
 
         onRegister();
 
-        navigate("/dashboard");
+        navigate('/dashboard');
       } else {
         alert(res.data.message);
       }
     } catch (err) {
       alert(err.response.data.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="container mx-auto p-4 flex items-center justify-center min-h-screen">
-      <form className="bg-white rounded-lg shadow-md p-8 max-w-md w-full" onSubmit={handleSubmit}>
+      <form
+        className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full"
+        onSubmit={handleSubmit}
+      >
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm self-start">
+  <h2 className="mt-6 text-center text-3xl font-bold leading-9 tracking-tight text-blue-900">
+    Welcome To Universal
+  </h2>
+</div>
+
+
+
+        {/* ... other form fields ... */}
+        <span class="block mb-1 text-md font-medium text-gray-700">User Name</span>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none focus:shadow-outline"
+          className=" appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none shadow-sm"
           type="text"
           name="username"
-          placeholder="Username"
+          placeholder="Your full name"
           onChange={handleChange}
         />
-        {errors.username && <p className="text-red-500 text-xs italic">{errors.username}</p>}
-        
+        {errors.username && (
+          <p className="text-red-500 text-xs italic">{errors.username}</p>
+        )}
+
+<span class="block mb-1 text-md font-medium text-gray-700">Your Email</span>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none focus:shadow-outline"
+          className=" appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none shadow-sm"
           type="email"
           name="email"
-          placeholder="Email"
+          placeholder="Ex. Universal@gmail.com"
           onChange={handleChange}
         />
-        {errors.email && <p className="text-red-500 text-xs italic">{errors.email}</p>}
-        
+        {errors.email && (
+          <p className="text-red-500 text-xs italic">{errors.email}</p>
+        )}
+
+<span class="block mb-1 text-md font-medium text-gray-700">Create a password</span>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none focus:shadow-outline"
+          className=" appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none shadow-sm"
           type="password"
           name="password"
           placeholder="Password"
           onChange={handleChange}
         />
-        {errors.password && <p className="text-red-500 text-xs italic">{errors.password}</p>}
-        
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none focus:shadow-outline"
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          onChange={handleChange}
-        />
-        {errors.confirmPassword && <p className="text-red-500 text-xs italic">{errors.confirmPassword}</p>}
-        
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          name="profession"
-          placeholder="Profession"
-          onChange={handleChange}
-        />
-        
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          name="license"
-          placeholder="License"
-          onChange={handleChange}
-        />
-        
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          <input
-            className="mr-2 leading-tight"
-            type="checkbox"
-            name="diaryblogAccess"
-            checked={user.diaryblogAccess}
-            onChange={handleCheckboxChange}
-          />
-          <span className="text-sm">Diary Blog Access</span>
-        </label>
-        
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          <input
-            className="mr-2 leading-tight"
-            type="checkbox"
-            name="typeitAccess"
-            checked={user.typeitAccess}
-            onChange={handleCheckboxChange}
-          />
-          <span className="text-sm">Type It Access</span>
-        </label>
-        
+        {errors.password && (
+          <p className="text-red-500 text-xs italic">{errors.password}</p>
+        )}
+
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-lg focus:outline-none shadow-sm"
           type="submit"
+          disabled={loading}
         >
-          Register
+          {loading ? (
+            <span className="flex items-center">
+              <span className="mr-2">Loading</span>
+              <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </span>
+          ) : (
+            'Register'
+          )}
         </button>
-        <Link className="block text-center mt-2 text-blue-500 hover:text-blue-800" to="/login">
+        <Link
+          className="block text-center mt-2 text-blue-500 hover:text-blue-800"
+          to="/login"
+        >
           Already have an account? Login
         </Link>
       </form>
-      
     </div>
   );
 }
