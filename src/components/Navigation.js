122580a849b9732d70b9327ddad7f4302b997215
athishsreeram <athishsreeram007@gmail.com>
2024-03-02 12:16:01 -0500

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -138,7 +138,7 @@ function Navigation({ isLoggedIn, setIsLoggedIn }) {
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             {isLoggedIn ? (
-              <div>
+              <div class="flex space-x-4">
                 <a
                   href="#"
                   className={`text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${
