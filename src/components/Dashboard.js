ebe0fbfe664eec4f8158b4be16ce187f6f9ba99c
Dinesh <dinesh.melapundi@gmail.com>
2024-03-02 20:47:57 +0530

    added a button to close and open left side menu bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,16 +2,36 @@
 import React, { useEffect, Suspense, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Menu } from "antd";
-import { HomeOutlined, StarOutlined, MessageOutlined } from "@mui/icons-material";
-import { SettingOutlined, LogoutOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  StarOutlined,
+  MessageOutlined,
+} from "@mui/icons-material";
+import {
+  SettingOutlined,
+  LogoutOutlined,
+  MenuOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faGripLinesVertical,
+} from "@fortawesome/free-solid-svg-icons";
 import { HelpOutline } from "@mui/icons-material";
 
-const DiaryBlogSpace = React.lazy(() => import("DiaryBlogSpace/DiaryBlogSpace"));
+const DiaryBlogSpace = React.lazy(() =>
+  import("DiaryBlogSpace/DiaryBlogSpace")
+);
 const FollowSpace = React.lazy(() => import("FollowSpace/FollowSpace"));
 
 const Dashboard = ({ onLogout, user, isLoggedIn }) => {
   const navigate = useNavigate();
   const [selectedKey, setSelectedKey] = useState(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isIconActive, setIsIconActive] = useState(false);
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -26,46 +46,102 @@ const Dashboard = ({ onLogout, user, isLoggedIn }) => {
     // Add any additional logout logic you may need.
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleOnMouseEnter = () => {
+    setIsIconActive(true);
+  };
+  const handleOnMouseLeave = () => {
+    setIsIconActive(false);
+  };
+
   return (
     isLoggedIn && (
       <div className="flex h-screen bg-gray-100">
         {/* Left Sidebar */}
-        <div className="w-1/5 bg-blue-800 text-white overflow-y-auto" style={{ width: '20%', height: '250%' }}>
-          <Menu
-            onClick={(e) => setSelectedKey(e.key)}
-            theme="dark"
-            mode="vertical"
-            className="h-full"
-          >
-            <Menu.Item key="home" icon={<HomeOutlined />}>
-              Dashboard
-            </Menu.Item>
-            <Menu.Item key="diaryBlogAdmin" icon={<StarOutlined />}>
-              DiaryBlog Admin
-            </Menu.Item>
-            <Menu.Item key="typeitAdmin" icon={<MessageOutlined />}>
-              Typelt Admin
-            </Menu.Item>
-            <Menu.Item key="followAdmin" icon={<MessageOutlined />}>
-              Follow Admin
-            </Menu.Item>
-            <Menu.Item key="help" icon={<HelpOutline />}>
-              Ask Admin
-            </Menu.Item>
-            <Menu.Item key="user" icon={<SettingOutlined />}>
-              User
-            </Menu.Item>
+        <div className="relative">
+          {isSidebarOpen && (
+            <div
+              className=" w-full bg-blue-800 text-white overflow-y-auto"
+              style={{ height: "100%" }}
+            >
+              <Menu
+                onClick={(e) => setSelectedKey(e.key)}
+                theme="dark"
+                mode="vertical"
+                className="h-full"
+              >
+                <Menu.Item key="home" icon={<HomeOutlined />}>
+                  Dashboard
+                </Menu.Item>
+                <Menu.Item key="diaryBlogAdmin" icon={<StarOutlined />}>
+                  DiaryBlog Admin
+                </Menu.Item>
+                <Menu.Item key="typeitAdmin" icon={<MessageOutlined />}>
+                  Typelt Admin
+                </Menu.Item>
+                <Menu.Item key="followAdmin" icon={<MessageOutlined />}>
+                  Follow Admin
+                </Menu.Item>
+                <Menu.Item key="help" icon={<HelpOutline />}>
+                  Ask Admin
+                </Menu.Item>
+                <Menu.Item key="user" icon={<SettingOutlined />}>
+                  User
+                </Menu.Item>
 
-            {isLoggedIn && (
-              <Menu.Item key="logout" icon={<LogoutOutlined />} onClick={logout}>
-                Logout
-              </Menu.Item>
+                {isLoggedIn && (
+                  <Menu.Item
+                    key="logout"
+                    icon={<LogoutOutlined />}
+                    onClick={logout}
+                  >
+                    Logout
+                  </Menu.Item>
+                )}
+              </Menu>
+            </div>
+          )}
+          <div
+            className="absolute top-1/2 transform -translate-y-1/2 ml-1 cursor-pointer"
+            style={{ left: "100%" }}
+          >
+            {isSidebarOpen ? (
+              <div
+                onClick={() => {
+                  toggleSidebar();
+                }}
+                style={{ fontSize: "20px" }}
+                onMouseLeave={() => {
+                  handleOnMouseLeave();
+                }}
+                onMouseEnter={() => {
+                  handleOnMouseEnter();
+                }}
+              >
+                {isIconActive ? (
+                  <FontAwesomeIcon icon={faAngleLeft} />
+                ) : (
+                  <FontAwesomeIcon icon={faGripLinesVertical} />
+                )}
+              </div>
+            ) : (
+              <div
+                onClick={() => {
+                  toggleSidebar();
+                }}
+                style={{ fontSize: "20px" }}
+              >
+                <FontAwesomeIcon icon={faAngleRight} />
+              </div>
             )}
-          </Menu>
+          </div>
         </div>
 
         {/* Right Content Area */}
-        <div className="w-4/5 p-4 bg-white">
+        <div className="w-full p-10 bg-white">
           <Suspense fallback={"loading..."}>
             <DiaryBlogSpace
               isLoggedIn={isLoggedIn}
