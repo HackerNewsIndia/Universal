b5cc6e4ac2a3a2f617b8ef768e161f95fda033c7
athishsreeram <athishsreeram007@gmail.com>
2024-03-03 22:58:36 -0500

    Update Navigation.js on click syntax fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -219,15 +219,13 @@ function Navigation({ isLoggedIn, setIsLoggedIn }) {
           >
             FAQ
           </Link>
-           {isLoggedIn && (
+           { isLoggedIn && (
            <Link
               to="/dashboard"
               className={`text-gray-100 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${
               selectedMobileMenuItem === "Dashboard" ? "bg-gray-900 text-white" : ""
-            }`}
-              onClick={() => {
-                onClick={() => handleMobileMenuLinkClick("Dashboard")}
-              }}
+             }`}
+             onClick={() => handleMobileMenuLinkClick("Dashboard")}
             >
                         Dashboard
             </Link>
