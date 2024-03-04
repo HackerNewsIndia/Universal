ad435e03d2ca036b6d6579ca38c8275bcf96f93b
IncPeopleConnect <136417291+IncPeopleConnect@users.noreply.github.com>
2024-03-03 21:45:48 -0500

    Update Navigation.js &&

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -134,7 +134,7 @@ function Navigation({ isLoggedIn, setIsLoggedIn }) {
                 >
                   FAQ
                 </Link>
-                {isLoggedIn ? (
+                {isLoggedIn && (
                 <Link
                         to="/dashboard"
                         className={`text-gray-100 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-md font-semibold`}
@@ -219,7 +219,7 @@ function Navigation({ isLoggedIn, setIsLoggedIn }) {
           >
             FAQ
           </Link>
-           {isLoggedIn ? (
+           {isLoggedIn && (
            <Link
               to="/dashboard"
               className={`text-gray-100 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${
