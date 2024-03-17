0199fe7836a3e53660882b4f0b21f4c98813a122
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2024-03-17 20:41:48 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,8 @@ import Product from "./Product";
 import Testimonials from "./Testimonials";
 import Pricing from "./Pricing";
 import Faq from "./Faq";
+import Footer from "./Footer";
+
 //import "./home.css"; // If you have additional custom styles
 
 const Home = () => {
@@ -156,43 +158,8 @@ const Home = () => {
 </section>
       
 
-      <footer className="border-t border-gray-200">
-        <div className="container flex flex-wrap items-center justify-center px-4 py-8 mx-auto lg:justify-between">
-          <div className="flex flex-wrap justify-center">
-            <ul className="flex items-center space-x-4">
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-              <li>Terms</li>
-            </ul>
-          </div>
-          <div className="flex justify-center mt-4 lg:mt-0">
-            {/* Similar structure for the social media icons */}
-            <a>
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6 text-blue-600" viewBox="0 0 24 24">
-          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-        </svg>
-      </a>
-      <a class="ml-3">
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6 text-blue-300" viewBox="0 0 24 24">
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-        </svg>
-      </a>
-      <a class="ml-3">
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6 text-pink-400" viewBox="0 0 24 24">
-          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-        </svg>
-      </a>
-      <a class="ml-3">
-        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-6 h-6 text-blue-500" viewBox="0 0 24 24">
-          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
-        </svg>
-      </a>
-          </div>
-        </div>
-      </footer>
+     
+      <Footer/>
     </div>
   );
 };
